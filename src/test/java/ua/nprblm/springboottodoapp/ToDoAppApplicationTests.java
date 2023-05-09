2f3fabe8e869df0c182918221929d32a4db39a18
nprblm <vitalik.bella@gmail.com>
2023-05-09 18:01:27 +0300

    add unversioned files

---
+++ src/test/java/ua/nprblm/springboottodoapp/ToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.nprblm.springboottodoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
