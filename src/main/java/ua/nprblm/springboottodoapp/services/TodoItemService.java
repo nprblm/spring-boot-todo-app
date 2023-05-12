5683a9aa0c0e4a9427a0aca851fedd5ff0ef8b01
nprblm <vitalik.bella@gmail.com>
2023-05-13 00:35:22 +0300

    fix services -> add interface TodoItemService and change class name from TodoItemService to TodoItemServiceImpl

--- src/main/java/ua/nprblm/springboottodoapp/services/TodoItemService.java
+++ src/main/java/ua/nprblm/springboottodoapp/services/TodoItemService.java
@@ -1,38 +1,15 @@
 package ua.nprblm.springboottodoapp.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import ua.nprblm.springboottodoapp.models.TodoItem;
-import ua.nprblm.springboottodoapp.repositories.TodoItemRepository;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Optional;
 
-@Service
-public class TodoItemService {
+public interface TodoItemService {
+    Iterable<TodoItem> getAll();
 
-    @Autowired
-    private TodoItemRepository todoItemRepository;
+    Optional<TodoItem> getById(Long id);
 
-    public Iterable<TodoItem> getAll() {
-        return todoItemRepository.findAll();
-    }
+    TodoItem save(TodoItem todoItem);
 
-    public Optional<TodoItem> getById(Long id) {
-        return todoItemRepository.findById(id);
-    }
-
-    public TodoItem save(TodoItem todoItem) {
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        if (todoItem.getId() == null) {
-            todoItem.setCreatedAt(dtf.format(LocalDateTime.now()));
-        }
-        todoItem.setUpdatedAt(dtf.format(LocalDateTime.now()));
-        return todoItemRepository.save(todoItem);
-    }
-
-    public void delete(TodoItem todoItem) {
-        todoItemRepository.delete(todoItem);
-    }
+    void delete(TodoItem todoItem);
 }
---
+++ src/main/java/ua/nprblm/springboottodoapp/services/TodoItemServiceImpl.java
@@ -0,0 +1,38 @@
+package ua.nprblm.springboottodoapp.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ua.nprblm.springboottodoapp.models.TodoItem;
+import ua.nprblm.springboottodoapp.repositories.TodoItemRepository;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Optional;
+
+@Service
+public class TodoItemServiceImpl implements TodoItemService {
+
+    @Autowired
+    private TodoItemRepository todoItemRepository;
+
+    public Iterable<TodoItem> getAll() {
+        return todoItemRepository.findAll();
+    }
+
+    public Optional<TodoItem> getById(Long id) {
+        return todoItemRepository.findById(id);
+    }
+
+    public TodoItem save(TodoItem todoItem) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        if (todoItem.getId() == null) {
+            todoItem.setCreatedAt(dtf.format(LocalDateTime.now()));
+        }
+        todoItem.setUpdatedAt(dtf.format(LocalDateTime.now()));
+        return todoItemRepository.saveAndFlush(todoItem);
+    }
+
+    public void delete(TodoItem todoItem) {
+        todoItemRepository.delete(todoItem);
+    }
+}
