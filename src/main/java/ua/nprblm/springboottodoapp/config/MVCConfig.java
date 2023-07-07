133f1a5f25fd141fc1d71b22ebf3522b6a2db795
nprblm <vitalik.bella@gmail.com>
2023-07-07 14:03:41 +0300

    Fix Not annotated parameter overrides @NonNullApi parameter warning

--- src/main/java/ua/nprblm/springboottodoapp/config/MVCConfig.java
+++ src/main/java/ua/nprblm/springboottodoapp/config/MVCConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
+@SuppressWarnings("NullableProblems")
 public class MVCConfig implements WebMvcConfigurer {
 
     @Override
